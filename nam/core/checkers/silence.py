8d0880d22aa5f7bda5a75f300ad071f015439f14
Pedro Algarvio <ufs@ufsoft.org>
2010-09-05 20:09:16 +0100

    The silence checkers now makes use of the database message kind's and set's it's source status accordingly.

--- nam/core/checkers/silence.py
+++ nam/core/checkers/silence.py
@@ -6,14 +6,20 @@ Created on 29 Aug 2010
 
 import gst
 import logging
+from datetime import datetime
 from twisted.internet import reactor
 from nam import component
+from nam.database import MessageKind
 from nam.event import NAMEvent
 
+STATUS_ERROR, STATUS_WARNING, STATUS_OK, STATUS_NONE = range(4)
 
 class AudioSilenceEvent(NAMEvent):
     def __init__(self, source_id, level, msg, levels):
-        self._args = [source_id, level, msg, levels]
+        self._args = [
+            datetime.now().strftime("%Y/%m/%d|%H:%M:%S"),
+            source_id, level, msg, levels
+        ]
 
 log = logging.getLogger(__name__)
 
@@ -57,13 +63,14 @@ class SilenceChecker(component.Component):
 
     def start(self):
         log.debug("%s component starting", self._component_name)
+        self.message_kinds = self.load_message_kinds()
         self.evtm.register_event_handler("AudioSilenceEvent",
                                          self.on_audio_silence_detected)
-#        self.prepare()
-        pass
 
     def stop(self):
         log.debug("%s component stoping", self._component_name)
+        self.evtm.deregister_event_handler("AudioSilenceEvent",
+                                           self.on_audio_silence_detected)
         self.prepare_reverse()
 
     def pause(self):
@@ -73,15 +80,22 @@ class SilenceChecker(component.Component):
     def shutdown(self):
         log.debug("%s component shutdown", self._component_name)
 
+    def load_message_kinds(self):
+        session = component.get("DatabaseManager").session()
+        message_kinds = {}
+        for kind in session.query(MessageKind).all():
+            message_kinds[kind.kind] = kind.id
+        return message_kinds
+
     def prepare(self):
         log.debug("%s component prepare", self._component_name)
         if self.gst_setup_complete:
             return
+
         self.source = component.get("SourcesManager").get(self.source_id)
 
         self.pipeline = self.source.pipeline
         self.bus = self.pipeline.get_bus()
-#        self.bus.add_signal_watch()
         self.bus.connect('message::element', self.check_bus_level_messages)
         self.tee = self.source.tee
         self.queue = self.gst_element_factory_make("queue")
@@ -101,59 +115,51 @@ class SilenceChecker(component.Component):
 
         log.debug("%s component prepared", self._component_name)
 
-#        def get_info():
-#            log.debug("%s component level state: %s", self._component_name,
-#                      self.level.get_state(0))
-#            log.debug(self.pipeline.get_state(0))
-##                log.debug(self.pipe2.get_state(0))
-#        from twisted.internet.task import LoopingCall
-#        state_check = LoopingCall(get_info)
-#        state_check.start(10)
-
     def prepare_reverse(self):
         log.debug("%s component prepare reverse", self._component_name)
         if not self.gst_setup_complete:
             return
-        self.gst_setup_complete = False
+#        self.gst_setup_complete = False
+        self.trigger_ok_both.active() and self.trigger_ok_both.cancel()
+        self.trigger_warning_both.active() and self.trigger_warning_both.cancel()
+        self.trigger_failure_both.active() and self.trigger_failure_both.cancel()
+        self.trigger_ok_left.active() and self.trigger_ok_left.cancel()
+        self.trigger_warning_left.active() and self.trigger_warning_left.cancel()
+        self.trigger_failure_left.active() and self.trigger_failure_left.cancel()
+        self.trigger_ok_right.active() and self.trigger_ok_right.cancel()
+        self.trigger_warning_right.active() and self.trigger_warning_right.cancel()
+        self.trigger_failure_right.active() and self.trigger_failure_right.cancel()
+        self.audio_failure_both_persists = False
+        self.audio_failure_left_persists = False
+        self.audio_failure_right_persists = False
+        self.audio_failure_both_persists_right_ok = False
+        self.audio_failure_both_persists_left_ok = False
 
     def gst_element_factory_make(self, gst_element_name, element_name=None):
         return self.source.gst_element_factory_make(gst_element_name, element_name)
-        if not element_name:
-            element_name = "%s-%d" % (gst_element_name, self.source_id)
-            if element_name in self.used_element_names:
-                n = 1
-                while True:
-                    element_name = "%s-%d-%d" % (gst_element_name, self.source_id, n)
-                    if element_name in self.used_element_names:
-                        n += 1
-                    else:
-                        break
-        self.used_element_names.append(element_name)
-        return gst.element_factory_make(gst_element_name, element_name)
 
-    def on_audio_silence_detected(self, source_id, level, msg, levels):
+    def on_audio_silence_detected(self, stamp, source_id, level, msg, levels):
         if source_id == self.source_id:
-            log.debug("SILENCE DETECTED - SRC ID: %s, LEVEL: %s, MSG: %s",
-                      source_id, level, msg)
-
-#    def trigger_audio_warning(self, msg):
-#        self.evtm.emit(AudioSilenceEvent(self.source_id, "WARN", msg))
-#        reactor.callLater(self.max_tolerance, self.trigger_audio_failure, msg)
-#
-#    def trigger_audio_failure(self, msg):
-#        self.evtm.emit(AudioSilenceEvent(self.source_id, "Error", msg))
+            log.debug("SILENCE DETECTED - SRC: %s, LEVEL: %s, MSG: %s",
+                      self.source.src_name, level, msg)
 
     def audio_warning_both(self, levels):
         self.trigger_ok_both.active() and self.trigger_ok_both.cancel()
         self.trigger_ok_left.active() and self.trigger_ok_left.cancel()
         self.trigger_ok_right.active() and self.trigger_ok_right.cancel()
         msg = "Audio Failure on Both Channels"
-        self.evtm.emit(AudioSilenceEvent(self.source_id, "WARN", "Possible %s" % msg, levels))
+        self.evtm.emit(AudioSilenceEvent(
+            self.source_id, self.message_kinds["WARNING"], "Possible %s" % msg, levels
+        ))
+        self.source.set_status(STATUS_WARNING)
         self.trigger_failure_both = reactor.callLater(
             self.max_tolerance, self.audio_failure_both, msg, levels
         )
     def audio_failure_both(self, msg, levels):
-        self.evtm.emit(AudioSilenceEvent(self.source_id, "ERROR", msg, levels))
+        self.evtm.emit(AudioSilenceEvent(
+            self.source_id, self.message_kinds["ERROR"], msg, levels
+        ))
+        self.source.set_status(STATUS_ERROR)
         self.audio_failure_both_persists = True
     def audio_ok_both(self, levels):
         msg = "Audio Resumed on Both channels"
@@ -163,7 +169,10 @@ class SilenceChecker(component.Component):
            self.audio_failure_left_persists or \
            self.trigger_failure_right.active() or \
            self.audio_failure_right_persists:
-             self.evtm.emit(AudioSilenceEvent(self.source_id, "OK", msg, levels))
+            self.evtm.emit(AudioSilenceEvent(
+                self.source_id, self.message_kinds["OK"], msg, levels
+            ))
+            self.source.set_status(STATUS_OK)
         self.audio_failure_both_persists = False
         self.audio_failure_left_persists = False
         self.audio_failure_right_persists = False
@@ -181,12 +190,18 @@ class SilenceChecker(component.Component):
         self.trigger_ok_left.active() and self.trigger_ok_left.cancel()
         self.trigger_ok_right.active() and self.trigger_ok_right.cancel()
         msg = "Audio Failure on Left Channel: %s" % levels
-        self.evtm.emit(AudioSilenceEvent(self.source_id, "WARN", "Possible %s" % msg, levels))
+        self.evtm.emit(AudioSilenceEvent(
+            self.source_id, self.message_kinds["WARNING"], "Possible %s" % msg, levels
+        ))
+        self.source.set_status(STATUS_WARNING)
         self.trigger_failure_left = reactor.callLater(
             self.max_tolerance, self.audio_failure_left, msg, levels
         )
     def audio_failure_left(self, msg, levels):
-        self.evtm.emit(AudioSilenceEvent(self.source_id, "ERROR", msg, levels))
+        self.evtm.emit(AudioSilenceEvent(
+            self.source_id, self.message_kinds["ERROR"], msg, levels
+        ))
+        self.source.set_status(STATUS_ERROR)
         self.audio_failure_left_persists = True
     def audio_ok_left(self, levels):
         msg = "Audio Resumed on Left channel: %s" % levels
@@ -194,9 +209,13 @@ class SilenceChecker(component.Component):
            self.audio_failure_left_persists or \
            self.trigger_failure_both.active() or \
            self.audio_failure_both_persists:
-            self.evtm.emit(AudioSilenceEvent(self.source_id, "OK", msg, levels))
+            self.evtm.emit(AudioSilenceEvent(
+                self.source_id, self.message_kinds["OK"], msg, levels
+            ))
         self.audio_failure_left_persists = False
+        self.source.set_status(STATUS_OK)
         if self.audio_failure_both_persists:
+            self.source.set_status(STATUS_ERROR)
             self.audio_failure_both_persists_left_ok = True
         self.trigger_warning_left.active() and self.trigger_warning_left.cancel()
         self.trigger_failure_left.active() and self.trigger_failure_left.cancel()
@@ -206,22 +225,32 @@ class SilenceChecker(component.Component):
         self.trigger_ok_left.active() and self.trigger_ok_left.cancel()
         self.trigger_ok_right.active() and self.trigger_ok_right.cancel()
         msg = "Audio Failure on Right Channel: %s" % levels
-        self.evtm.emit(AudioSilenceEvent(self.source_id, "WARN", "Possible %s" % msg, levels))
+        self.evtm.emit(AudioSilenceEvent(
+            self.source_id, self.message_kinds["WARNING"], "Possible %s" % msg, levels
+        ))
         self.trigger_failure_right = reactor.callLater(
             self.max_tolerance, self.audio_failure_right, msg, levels
         )
+        self.source.set_status(STATUS_WARNING)
     def audio_failure_right(self, msg, levels):
-        self.evtm.emit(AudioSilenceEvent(self.source_id, "ERROR", msg, levels))
+        self.evtm.emit(AudioSilenceEvent(
+            self.source_id, self.message_kinds["ERROR"], msg, levels
+        ))
         self.audio_failure_right_persists = True
+        self.source.set_status(STATUS_ERROR)
     def audio_ok_right(self, levels):
         msg = "Audio Resumed on Right channel: %s" % levels
         if self.trigger_failure_right.active() or \
            self.audio_failure_right_persists or \
            self.trigger_failure_both.active() or \
            self.audio_failure_both_persists:
-            self.evtm.emit(AudioSilenceEvent(self.source_id, "OK", msg, levels))
+            self.evtm.emit(AudioSilenceEvent(
+                self.source_id, self.message_kinds["OK"], msg, levels
+            ))
         self.audio_failure_right_persists = False
+        self.source.set_status(STATUS_OK)
         if self.audio_failure_both_persists:
+            self.source.set_status(STATUS_ERROR)
             self.audio_failure_both_persists_right_ok = True
         self.trigger_warning_right.active() and self.trigger_warning_right.cancel()
         self.trigger_failure_right.active() and self.trigger_failure_right.cancel()
